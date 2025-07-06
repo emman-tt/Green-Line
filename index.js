5dd85cb5c68b8dd1c2b981bbad601bb2d107fd70
Your Name <Emmanuelaquarius2006@gmail.com>
2025-07-06 00:32:51 +0000

    `Refactored index.html, index.js, and style.css files with added IDs, element retrieval, and GSAP animations.`

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
 let positionedImage = document.querySelector('.positioned-image')
+let movingDrink = document.getElementById("movingDrink")
 let slides = document.querySelector('.slides');
 let cards = document.querySelectorAll('.review-cards');
 let nextBtn = document.getElementById('nextBtn');
 let prevBtn = document.getElementById('prevBtn');
 let deviceWidth = window.innerWidth;
-console.log(deviceWidth);
+let drinkDisplay1 = document.getElementById("drinkDisplay1")
+let drinkDisplay2 = document.getElementById("drinkDisplay2")
+let drinkDisplay3 = document.getElementById("drinkDisplay3")
+let circle = document.getElementById("circle")
+let circle1 = document.getElementById("circle1")
+let circle2 = document.getElementById("circle2")
+let tween;
+
+
+drinkDisplay1.style.display = "none"
+drinkDisplay2.style.display = "none"
+drinkDisplay3.style.display = "none"
+
+
+
+
+function movingDrinkReturn() {
+  movingDrink.style.zIndex = 10
+  drinkDisplay1.style.display = "none"
+  drinkDisplay2.style.display = "none"
+  drinkDisplay3.style.display = "none"
+  
+}
+
+
+circle.addEventListener("click", () => {
+ drinkDisplay3.style.display = "none"
+ drinkDisplay2.style.display = "none"
+ drinkDisplay1.style.display = "block"
+ movingDrink.style.zIndex = -1
+})
+
+circle1.addEventListener("click", () => {
+ drinkDisplay2.style.display = "block"
+ drinkDisplay1.style.display = "none"
+ drinkDisplay3.style.display = "none"
+ movingDrink.style.zIndex = -1
+})
+
+circle2.addEventListener("click", () => {
+ drinkDisplay3.style.display = "block"
+ drinkDisplay2.style.display = "none"
+ drinkDisplay1.style.display = "none"
+movingDrink.style.zIndex = -1
+
+})
+
+
+
 
 
 let currentIndex = 0;
@@ -40,36 +89,41 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (deviceWidth >= 610 && deviceWidth <= 900  ) {
         
-     gsap.to('.movingDrink', {
-    y: 650,
+  tween =   gsap.to('.movingDrink', {
+    y: 620,
     scale: 2,
     x: -100,
      ease: 'linear',
     // yoyo: true,
-    duration: 50,
+    duration: 5,
+    onComplete: () =>{movingDrinkReturn()},
+  
 
     scrollTrigger: {
       trigger: '.movingDrink',
-      start: 'bottom 20%',
+      start: 'bottom 60%',
       end: 'top 20%',
-      scrub: 5
+      scrub: 5,
+      
     }
   })
+
     }
 
     if (deviceWidth >= 905 && deviceWidth <= 1200  ) {
-     gsap.to('.movingDrink', {
+   tween =  gsap.to('.movingDrink', {
     y: 780,
     scale: 2,
     x: -100,
     transform:"none",
      ease: 'linear',
     // yoyo: true,
-    duration: 50,
+    duration: 5,
+     onComplete: () =>{movingDrinkReturn()},
 
     scrollTrigger: {
       trigger: '.movingDrink',
-      start: 'bottom 30%',
+      start: 'bottom 60%',
       end: 'top 20%',
       scrub: 5
     }
@@ -83,7 +137,8 @@ document.addEventListener('DOMContentLoaded', () => {
     transform:"none",
      ease: 'linear',
     // yoyo: true,
-    duration: 50,
+    duration: 5,
+     onComplete: () =>{movingDrinkReturn()},
 
     scrollTrigger: {
       trigger: '.movingDrink',
@@ -101,7 +156,8 @@ document.addEventListener('DOMContentLoaded', () => {
     transform:"none",
      ease: 'linear',
     // yoyo: true,
-    duration: 50,
+    duration: 5,
+     onComplete: () =>{movingDrinkReturn()},
 
     scrollTrigger: {
       trigger: '.movingDrink',
@@ -120,7 +176,8 @@ document.addEventListener('DOMContentLoaded', () => {
     transform:"none",
      ease: 'linear',
     // yoyo: true,
-    duration: 50,
+    duration: 5,
+     onComplete: () =>{movingDrinkReturn()},
 
     scrollTrigger: {
       trigger: '.movingDrink',
@@ -138,7 +195,8 @@ document.addEventListener('DOMContentLoaded', () => {
     transform:"none",
      ease: 'linear',
     // yoyo: true,
-    duration: 50,
+    duration: 5,
+     onComplete: () =>{movingDrinkReturn()},
 
     scrollTrigger: {
       trigger: '.movingDrink',
